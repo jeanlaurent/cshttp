1625849da5c587e8db2d1e9f1039b52d4ac35174
Jean-Laurent de Morlhon <jeanlaurent@morlhon.net>
2016-04-27 10:40:02 +0100

    Initial blunt http c# server
    Signed-off-by: Jean-Laurent de Morlhon <jeanlaurent@morlhon.net>

---
+++ CsHttp/HttpServer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace CsHttp
+{
+  public class HttpServer
+  {
+    private readonly HttpListener _httpListener;
+    private readonly Dictionary<string, Action<HttpListenerContext>> _routes;
+
+    public HttpServer()
+    {
+      _httpListener = new HttpListener();
+      _routes = new Dictionary<string, Action<HttpListenerContext>>();
+    }
+
+    public void AddRoute(String path, Action<HttpListenerContext> action)
+    {
+       _routes.Add(path, action);
+    }
+
+    public async void Listen(int port)
+    {
+        _httpListener.Prefixes.Add($"http://*:{port}/");
+        _httpListener.Start();
+        for (;;)
+        {
+          var context = _httpListener.GetContext();
+          await Dispatch(context);
+        }
+    }
+
+    private async Task<bool> Dispatch(HttpListenerContext context)
+    {
+        Action<HttpListenerContext> action;
+        if (_routes.TryGetValue(context.Request.Url.LocalPath, out action))
+        {
+          await Task.Run(() => action.Invoke(context));
+        }
+        else
+        {
+          await Task.Run(() =>
+          {
+            context.Response.StatusCode = 404;
+            context.Response.Close();
+          });
+        }
+      return true;
+    }
+
+  }
+}
---
+++ CsHttp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+
+
+namespace CsHttp
+{
+
+  class Program
+  {
+
+    static void Main(string[] args)
+    {
+      // named pipes on windows : \\<machine_address>\pipe\<pipename>
+      var httpServer = new HttpServer();
+      httpServer.AddRoute("/", (context) =>
+      {
+          context.Response.StatusCode = 200;
+          context.Response.Close();
+      });
+      httpServer.AddRoute("/foobar", (context) =>
+      {
+        context.Response.StatusCode = 200;
+        context.Response.Headers.Add("Content-Type", "application/json");
+        var bytes = Encoding.UTF8.GetBytes("{\"name\":\"foo\"}");
+        context.Response.OutputStream.Write(bytes, 0, bytes.Length);
+        context.Response.Close();
+      });
+      httpServer.Listen(8080);
+    }
+  }
+}
+
+
